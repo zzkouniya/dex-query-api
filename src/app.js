d44e1dc703c41b7f6f7a9987f605fe45b1ec9555
techland <techland.co@gmail.com>
2020-10-24 20:51:47 +0800

    feat: use winston to log request

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const app = express();
+const expressWinston = require('express-winston');
+const winston = require('winston');
+
 const helmet = require('helmet');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -8,15 +10,38 @@ const ordersRouter = require('./orders/router');
 const cellsRouter = require('./cells/router');
 const swagger = require('./swagger/middleware');
 
+const app = express();
+
 app.use(helmet());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(boolParser());
 app.use('/api-docs', swagger.middleware);
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console(),
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json(),
+  ),
+}));
+
 app.use(
   ordersRouter,
   cellsRouter,
 );
+
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console(),
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json(),
+  ),
+}));
 
 module.exports = app;
