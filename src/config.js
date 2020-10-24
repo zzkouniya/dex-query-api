a1bec15ff70dd469cb5077794d86f5bffead76bd
techland <techland.co@gmail.com>
2020-10-24 19:12:29 +0800

    chore: format config

--- src/config.js
+++ src/config.js
@@ -7,12 +7,14 @@ const contracts = {
   orderLock: {
     codeHash: process.env.ORDER_LOCK_CODE_HASH,
     hashType: process.env.ORDER_LOCK_HASH_TYPE,
-  }
-}
+  },
+};
 
 const indexer = {
   dataPath: process.env.INDEXER_FOLDER_PATH || 'http://127.0.0.1:8554',
   nodeUrl: process.env.CKB_NODE_RPC_URL || 'http://localhost:8114',
-}
+};
 
-module.exports = {env, port, contracts, indexer};
+module.exports = {
+  env, port, contracts, indexer,
+};
