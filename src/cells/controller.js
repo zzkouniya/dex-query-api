2cc2bcdea6874fe9fa371178410d6363b8142b60
techland <techland.co@gmail.com>
2020-10-27 20:08:02 +0800

    chore: remove dead code

--- src/cells/controller.js
+++ src/cells/controller.js
@@ -107,43 +107,6 @@ class Controller {
       res.status(500).send();
     }
   }
-
-  async getSUDTBalance(req, res) {
-    const {
-      lock_code_hash,
-      lock_hash_type,
-      lock_args,
-      type_code_hash,
-      type_hash_type,
-      type_args,
-    } = req.query;
-
-    if (!isValidScript(lock_code_hash, lock_hash_type, lock_args) || !isValidScript(type_code_hash, type_hash_type, type_args)) {
-      return res.status(400).json({ error: 'requires both lock and type scripts to be specified as parameters' });
-    }
-
-    const queryOptions = {
-      lock: {
-        code_hash: lock_code_hash,
-        hash_type: lock_hash_type,
-        args: lock_args,
-      },
-      type: {
-        code_hash: type_code_hash,
-        hash_type: type_hash_type,
-        args: type_args,
-      },
-    };
-
-    try {
-      const cells = await indexer.collectCells(queryOptions);
-      const balance = cells.reduce((total, cell) => total + formatter.readBigUInt128LE(cell.capacity), BigInt(0));
-      res.status(200).json(balance.toString());
-    } catch (err) {
-      console.error(err);
-      res.status(500).send();
-    }
-  }
 }
 
 const collectCellsBySudtAmount = (cells, amount) => {
