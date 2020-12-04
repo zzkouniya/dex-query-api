dada8c0bf3fb5c7336d1f9c117c0d3ae2e4c6687
zzkouniya <dev.jianhui@gmail.com>
2020-12-04 11:55:18 +0800

    fix: Read order data 64 to 128

--- src/component/formatter.ts
+++ src/component/formatter.ts
@@ -24,8 +24,9 @@ export class CkbUtils {
 
     let price: bigint;
     try {
-      const priceBuf: Buffer = Buffer.from(hex.slice(66, 98), "hex");
-      price = priceBuf.readBigInt64LE();
+      price = this.parseAmountFromLeHex(hex.slice(66, 98));
+      // const priceBuf: Buffer = Buffer.from(hex.slice(66, 98), "hex");
+      // price = priceBuf.readBigInt64LE();
     } catch (error) {
       price = null;
     }
