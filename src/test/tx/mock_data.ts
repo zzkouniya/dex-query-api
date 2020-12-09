462414861b13431173842fb7bfbaab1d9a918036
zzkouniya <dev.jianhui@gmail.com>
2020-12-09 19:31:40 +0800

    test: Add tx-by-hash test case

--- src/test/tx/mock_data.ts
+++ src/test/tx/mock_data.ts
@@ -1,6 +1,7 @@
+import { CkbUtils } from '../../component'
 import { CkbTransactionWithStatusModel } from '../../model/ckb/ckb_transaction_with_status'
 
-export const tx: CkbTransactionWithStatusModel = {
+export const ckbTx: CkbTransactionWithStatusModel = {
   transaction: {
     cellDeps: [],
     inputs: [ 
@@ -35,7 +36,7 @@ export const tx: CkbTransactionWithStatusModel = {
     outputsData: [ '0x', '0x' ],
     headerDeps: [],
     hash: '0x34c5ba66e5be46e4b78d3dafe6a9f124912af64bad2f3a3ff53e1c684b7694c7',
-    version: "",
+    version: "0x0",
     witnesses: []
   },
   txStatus: {
@@ -46,7 +47,7 @@ export const tx: CkbTransactionWithStatusModel = {
 }
 
 
-export const preTx: CkbTransactionWithStatusModel = {
+export const preCkbTx: CkbTransactionWithStatusModel = {
   transaction: {
     cellDeps: [],
     inputs: [ 
@@ -72,7 +73,7 @@ export const preTx: CkbTransactionWithStatusModel = {
     outputsData: [ '0x', '0x' ],
     headerDeps: [],
     hash: 'hash0',
-    version: "",
+    version: "0x0",
     witnesses: []
   },
   txStatus: {
@@ -82,4 +83,124 @@ export const preTx: CkbTransactionWithStatusModel = {
 }
 
 
+export const sudtTx = {
+  transaction: {
+    cellDeps: [],
+    inputs: [ 
+      {
+        previousOutput: {
+          txHash: 'hash0',
+          index: '0x0'
+        },
+        since: '0x0'
+      },
+      {
+        previousOutput: {
+          txHash: 'hash0',
+          index: '0x1'
+        },
+        since: '0x0'
+      }
+    ],
+    outputs: [ 
+      {
+        lock: {
+          "args": "0x252dae0a4b9d9b80f504f6418acd2d364c0c59cd",
+          "codeHash": "0x58c5f491aba6d61678b7cf7edf4910b1f5e00ec0cde2f42e0abb4fd9aff25a62",
+          "hashType": "type"
+        },
+        type: {
+          "args": "0x6fe3733cd9df22d05b8a70f7b505d0fb67fb58fb88693217135ff5079713e902",
+          "codeHash": "0xc5e5dcf215925f7ef4dfaf5f4b4f105bc321c02776d6e7d52a1db3fcd9d011a4",
+          "hashType": "type"
+        },
+        capacity: `0x${BigInt(18700000000n).toString(16)}`
+      },
+      {
+        lock: {
+          "args": "0x988485609e16d5d5c62be0a4ae12b665fefcb448",
+          "codeHash": "0x58c5f491aba6d61678b7cf7edf4910b1f5e00ec0cde2f42e0abb4fd9aff25a63",
+          "hashType": "type"
+        },
+        type: {
+          "args": "0x6fe3733cd9df22d05b8a70f7b505d0fb67fb58fb88693217135ff5079713e902",
+          "codeHash": "0xc5e5dcf215925f7ef4dfaf5f4b4f105bc321c02776d6e7d52a1db3fcd9d011a4",
+          "hashType": "type"
+        },
+        capacity: `0x${BigInt(18700000000n).toString(16)}`
+      },
+      {
+        lock: {
+          codeHash: '0x58c5f491aba6d61678b7cf7edf4910b1f5e00ec0cde2f42e0abb4fd9aff25a63',
+          hashType: 'type',
+          args: '0x988485609e16d5d5c62be0a4ae12b665fefcb448'
+        },
+        type: null,
+        capacity: `0x${BigInt(1999999995000n).toString(16)}`
+      }
+    ],
+    outputsData: [ 
+      CkbUtils.formatOrderData(4000000000n, BigInt(1), BigInt(1), false), 
+      CkbUtils.formatOrderData(6000000000n, BigInt(1), BigInt(1), false),
+      '0x' 
+    ],
+    headerDeps: [],
+    hash: '0x34c5ba66e5be46e4b78d3dafe6a9f124912af64bad2f3a3ff53e1c684b7694c7',
+    version: "",
+    witnesses: []
+  },
+  txStatus: {
+    blockHash: 'block1',
+    status: 'committed'
+  }
+}
 
+
+export const preSudtTx: CkbTransactionWithStatusModel = {
+  transaction: {
+    cellDeps: [],
+    inputs: [ 
+      {
+        previousOutput: {
+          txHash: 'hash0',
+          index: '0x0'
+        },
+        since: '0x0'
+      }
+    ],
+    outputs: [ 
+      {
+        lock: {
+          codeHash: '0x58c5f491aba6d61678b7cf7edf4910b1f5e00ec0cde2f42e0abb4fd9aff25a63',
+          hashType: 'type',
+          args: '0x988485609e16d5d5c62be0a4ae12b665fefcb448'
+        },
+        type: null,
+        capacity: `0x${BigInt(2000000000000n).toString(16)}`
+      },
+      {
+        lock: {
+          "args": "0x988485609e16d5d5c62be0a4ae12b665fefcb448",
+          "codeHash": "0x58c5f491aba6d61678b7cf7edf4910b1f5e00ec0cde2f42e0abb4fd9aff25a63",
+          "hashType": "type"
+        },
+        type: {
+          "args": "0x6fe3733cd9df22d05b8a70f7b505d0fb67fb58fb88693217135ff5079713e902",
+          "codeHash": "0xc5e5dcf215925f7ef4dfaf5f4b4f105bc321c02776d6e7d52a1db3fcd9d011a4",
+          "hashType": "type"
+        },
+        capacity: `0x${BigInt(18700000000n).toString(16)}`
+      }
+    ],
+    outputsData: [ '0x', 
+      CkbUtils.formatOrderData(10000000000n, BigInt(1), BigInt(1), false)],
+    headerDeps: [],
+    hash: 'hash0',
+    version: "0x0",
+    witnesses: []
+  },
+  txStatus: {
+    blockHash: 'block0',
+    status: 'committed'
+  }
+}
--- src/test/tx/tx_controller.spec.ts
+++ src/test/tx/tx_controller.spec.ts
@@ -14,7 +14,7 @@ import { HashType } from '@ckb-lumos/base';
 import TxService from '../../modules/tx/tx_service';
 import TxController from '../../modules/tx/tx_controller';
 import { MockRepository, MockRepositoryFactory } from '../mock_repository_factory';
-import { preTx, tx } from './mock_data';
+import { ckbTx, preCkbTx, preSudtTx, sudtTx } from './mock_data';
 import CkbTransactionWithStatusModelWrapper from '../../model/ckb/ckb_transaction_with_status';
 
 describe('Balance controller', () => {
@@ -29,15 +29,11 @@ describe('Balance controller', () => {
     hash_type: <HashType>'type',
     args: '0x988485609e16d5d5c62be0a4ae12b665fefcb448',
   };
-  // const orderLock = {
-  //   code_hash: contracts.orderLock.codeHash,
-  //   hash_type: contracts.orderLock.hashType,
-  //   args: utils.computeScriptHash(lock),
-  // };
+
   const sudtType = {
     code_hash: '0xc5e5dcf215925f7ef4dfaf5f4b4f105bc321c02776d6e7d52a1db3fcd9d011a4',
-    hash_type: 'data',
-    args: '0x9668cc9f8652ff7f1821dd265a54bf4b9c4a6345e3457bbfbcda95fe899a6097',
+    hash_type: 'type',
+    args: '0x6fe3733cd9df22d05b8a70f7b505d0fb67fb58fb88693217135ff5079713e902',
   };
 
   beforeEach(() => {
@@ -136,7 +132,7 @@ describe('Balance controller', () => {
     });
 
     describe('valid requests', () => {
-      describe('with no lock script', () => {
+      describe('get ckb transaction', () => {
         beforeEach(async () => {
           req.query = {
             lock_code_hash: lock.code_hash,
@@ -151,18 +147,56 @@ describe('Balance controller', () => {
   
           mock_repository.mockGetTransactionByHash()
             .withArgs("hash1")
-            .resolves(new CkbTransactionWithStatusModelWrapper(tx));
+            .resolves(new CkbTransactionWithStatusModelWrapper(ckbTx));
           mock_repository.mockGetTransactions()
-            .resolves([new CkbTransactionWithStatusModelWrapper(preTx)]);
+            .resolves([new CkbTransactionWithStatusModelWrapper(preCkbTx)]);
   
           await controller.getTransactionsByTxHash(req, res, next);
         });
-        it('returns balance', () => {
+        it('returns transaction', () => {
           res.status.should.have.been.calledWith(200);
           res.json.should.have.been.calledWith({
             status: 'committed',
             transaction_fee: '1',
             amount: '-200',
+            to: '0x252dae0a4b9d9b80f504f6418acd2d364c0c59cd',
+            from: '0x988485609e16d5d5c62be0a4ae12b665fefcb448',
+            hash: 'hash1',
+            block_no: 1
+          });
+        });
+      });
+
+      describe('get sudt transaction', () => {
+        beforeEach(async () => {
+          req.query = {
+            lock_code_hash: lock.code_hash,
+            lock_hash_type: lock.hash_type,
+            lock_args: lock.args,
+            type_code_hash: sudtType.code_hash,
+            type_hash_type: sudtType.hash_type,
+            type_args: sudtType.args,
+            tx_hash: "hash1"
+          };
+  
+          mock_repository.mockGetblockNumberByBlockHash()
+            .withArgs("block1")
+            .resolves(1);
+  
+          mock_repository.mockGetTransactionByHash()
+            .withArgs("hash1")
+            .resolves(new CkbTransactionWithStatusModelWrapper(sudtTx));
+          mock_repository.mockGetTransactions()
+            .resolves([new CkbTransactionWithStatusModelWrapper(preSudtTx)]);
+  
+          await controller.getTransactionsByTxHash(req, res, next);
+        });
+        it('returns transaction', () => {
+          res.status.should.have.been.calledWith(200);
+          res.json.should.have.been.calledWith({
+            status: 'committed',
+            transaction_fee: '5000',
+            amount: '-4000000000',
             to: '0x252dae0a4b9d9b80f504f6418acd2d364c0c59cd',
             from: '0x988485609e16d5d5c62be0a4ae12b665fefcb448',
             hash: 'hash1',
