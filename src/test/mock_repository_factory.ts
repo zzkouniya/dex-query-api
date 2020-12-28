4429fe4aaa758610ff79f89dfa536c2260b39436
zjh <zjh@zjhdeiMac-Pro.local>
2020-12-28 19:00:05 +0800

    fix: test cases

--- src/test/mock_repository_factory.ts
+++ src/test/mock_repository_factory.ts
@@ -12,7 +12,10 @@ export class MockRepositoryFactory {
 }
 
 export class MockRepository implements DexRepository {
-  getInputOutPointFromTheTxPool: () => Promise<Map<string, CkbTransactionWithStatusModelWrapper>>
+  async getInputOutPointFromTheTxPool (): Promise<Map<string, CkbTransactionWithStatusModelWrapper>> {
+    return new Map()
+  }
+
   async tip (): Promise<number> {
     return null
   }
@@ -83,5 +86,10 @@ export class MockRepository implements DexRepository {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   mockGetBlockTimestampByHash (): sinon.SinonStub<any[], any> | sinon.SinonStub<unknown[], unknown> {
     return sinon.stub(this, 'getBlockTimestampByHash')
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  mockGetInputOutPointFromTheTxPool (): sinon.SinonStub<any[], any> | sinon.SinonStub<unknown[], unknown> {
+    return sinon.stub(this, 'getInputOutPointFromTheTxPool')
   }
 }
