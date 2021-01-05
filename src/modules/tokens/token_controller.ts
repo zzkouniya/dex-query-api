7fe4a256d379b0a01ecd2ba761ac1b8a78612681
zjh <zjh@zjhdeiMac-Pro.local>
2021-01-05 19:50:06 +0800

    cell info

---
+++ src/modules/tokens/token_controller.ts
@@ -0,0 +1,91 @@
+import express from 'express'
+import { inject, LazyServiceIdentifer } from 'inversify'
+import { controller, httpGet } from 'inversify-express-utils'
+import { ApiOperationGet, ApiPath } from 'swagger-express-ts'
+import { DexLogger } from '../../component'
+import { modules } from '../../ioc'
+import TokenService from './token_service'
+
+@ApiPath({
+  path: '/',
+  name: 'Token',
+  security: { basicAuth: [] }
+})
+@controller('/')
+export default class TokenController {
+  private readonly logger: DexLogger
+  constructor (
+    @inject(new LazyServiceIdentifer(() => modules[TokenService.name]))
+    private readonly tokenService: TokenService
+  ) {
+    this.logger = new DexLogger(TokenController.name)
+  }
+
+  @ApiOperationGet({
+    path: 'tokens/typeHashOrAddress',
+    description: 'query by typeHash',
+    summary: 'query by typeHash',
+    parameters: {
+      query: {
+        type_code_hash: {
+          name: 'typeHashOrAddress',
+          type: 'string',
+          required: true,
+          description: ''
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Success'
+      },
+      400: { description: 'Parameters fail' }
+    }
+  })
+  @httpGet('tokens/typeHashOrAddress')
+  async typeHash (req: express.Request, res: express.Response): Promise<void> {
+    const typeHashOrAddress = <string>req.query.typeHashOrAddress
+    try {
+      const tokenInfo = await this.tokenService.getTokesByTypeHashOrAddress(typeHashOrAddress)
+      res.status(200).json(tokenInfo)
+    } catch (error) {
+      this.logger.error(error)
+    }
+  }
+
+  @ApiOperationGet({
+    path: 'tokens/cellInfo',
+    description: 'query cell info',
+    summary: 'query cell info',
+    parameters: {
+      query: {
+        type_code_hash: {
+          name: 'typeHash',
+          type: 'string',
+          required: true,
+          description: ''
+        }
+      }
+    },
+    responses: {
+      200: {
+        description: 'Success'
+      },
+      400: { description: 'Parameters fail' }
+    }
+  })
+  @httpGet('tokens/cellInfo')
+  async cellInfo (req: express.Request, res: express.Response): Promise<void> {
+    const typeHash = <string>req.query.typeHash
+    try {
+      const cellInfo = await this.tokenService.getCellInfoByTypeHash(typeHash)
+      if (!cellInfo) {
+        res.status(400).json({ error: 'cell info does not exist' })
+        return
+      }
+      res.status(200).json(cellInfo)
+    } catch (error) {
+      this.logger.error(error)
+    }
+  }
+}
