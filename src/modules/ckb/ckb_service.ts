0d3ce68c95738be35ce629dfbe10245b82b5ae8a
zjh <zjh@zjhdeiMac-Pro.local>
2021-01-21 19:11:07 +0800

    feat(server): remove intersection

--- src/modules/ckb/ckb_service.ts
+++ src/modules/ckb/ckb_service.ts
@@ -39,9 +39,12 @@ export default class CkbService {
   }
 
   async getblockNumberByBlockHash (blockHash: string): Promise<number> {
+    console.log(blockHash)
+
     const req = []
     req.push(['getBlock', blockHash])
     const block = await this.ckbNode.rpc.createBatchRequest(req).exec()
+
     const blockNumber = parseInt(block[0].header.number, 16)
 
     return blockNumber
